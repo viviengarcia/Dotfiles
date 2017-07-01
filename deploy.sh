f5063f099d77867d77cefd97b9b7310eee9730a9
Vivien Garcia <garcia.vivien@gmail.com>
2017-07-01 23:26:07 +0200

    moved deployment logic in its own file

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# DEPLOY.SH
+# This shell script creates symbolic links to Bash configuration files, so the ".dotfiles" git repo isn't at the root of my Home directory but more logicaly in my projects folder. Another aim is to help separate Bash configuration and System installation, because linking files isn't in the install script anymore
+
+
+echo "Deploying dotfiles..."
+
+# Dotfiles deployment
+mkdir ~/.bash
+ln -sfv "~/.dotfiles/conf/bash_profile" "~/.bash_profile"
+ln -sfv "~/.dotfiles/conf/aliases" "~/.bash/aliases"
+ln -sfv "~/.dotfiles/conf/exports" "~/.bash/exports"
+ln -sfv "~/.dotfiles/conf/extras" "~/.bash/extras"
+ln -sfv "~/.dotfiles/conf/functions" "~/.bash/functions"
+ln -sfv "~/.dotfiles/conf/path" "~/.bash/path"
+ln -sfv "~/.dotfiles/conf/prompt" "~/.bash/prompt"
+
+ln -sfv "~/.dotfiles/conf/mackup.cfg" "~/.mackup.cfg"
+
+# Done
+echo "Done. That was fast!"
--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 
 echo "Setting up your Mac..."
 
-# Dotfiles deployment
-ln -sfv "~/.dotfiles/conf/bash_profile" "~/.bash_profile"
-mkdir ~/.bash
-ln -sfv "~/.dotfiles/conf/aliases" "~/.bash/aliases"
-ln -sfv "~/.dotfiles/conf/mackup.cfg" "~/.mackup.cfg"
-
 # Set Bash settings
 source ~/.bash_profile
+
+# Installing software via Homebrew and Homebrew/Cask
+source ~/Dotfiles/cask
 
 # Set Applications Preferences via mackup
 source ~/.mackup.cfg
 mackup restore
 
+
 # Set OS X preferences
-source ~/.dotfiles/.osx
+source ~/Dotfiles/osx
+source ~/Dotfiles/dock
+source ~/Dotfiles/privacy_fix
 
 # Done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
