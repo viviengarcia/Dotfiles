778a8faaa0980aea2090dda235c7b31224f17c95
Vivien Garcia <garcia.vivien@gmail.com>
2017-07-05 18:06:24 +0200

    changed steps order

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@
 echo "Setting up your Mac..."
 
 # Établissement des liens symboliques
-source ~/Dotfiles/symlinks.sh
-
-# Installing software via Homebrew and Homebrew/Cask
-source ~/Dotfiles/cask
+source "~/Dotfiles/symlinks.sh"
 
 # Set Bash settings
 source ~/.bash_profile
 
-# Set Applications Preferences via mackup
-source ~/.mackup.cfg
-mackup restore
+# Installing software via Homebrew and Homebrew/Cask
+source ~/Dotfiles/cask
 
 # Set OS X preferences
 source ~/Dotfiles/osx
 source ~/Dotfiles/dock
 source ~/Dotfiles/privacy_fix
+
+# Set Applications Preferences via mackup
+source ~/.mackup.cfg
+mackup restore
 
 # Done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
