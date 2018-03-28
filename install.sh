df8a8fb6012df0ceaeb999a5cf21ab4b460407dc
Vivien Garcia <viviengarcia@users.noreply.github.com>
2018-03-28 17:32:49 +0200

    New Project Path

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 echo "Setting up your Mac..."
 
 # Établissement des liens symboliques
-source "~/Dotfiles/symlinks.sh"
+source "~/Digital\ Creation/Dotfiles/symlinks.sh"
 
 # Set Bash settings
 source ~/.bash_profile
 
 # Installing software via Homebrew and Homebrew/Cask
-source ~/Dotfiles/cask
+source ~/Digital\ Creation/Dotfiles/cask
 
 # Set OS X preferences
-source ~/Dotfiles/osx
-source ~/Dotfiles/dock
-source ~/Dotfiles/privacy_fix
+#source ~/Digital\ Creation/Dotfiles/osx
+source ~/Digital\ Creation/Dotfiles/dock
+source ~/Digital\ Creation/Dotfiles/privacy_fix
 
 # Set Applications Preferences via mackup
 source ~/.mackup.cfg
--- symlinks.sh
+++ symlinks.sh
@@ -5,25 +5,25 @@
 # S Y M L I N K S
 
 #--------------------------------------------------------------------------#
-# This shell script creates symbolic links to Bash configuration files, so the ".dotfiles" git repo isn't at the root of my Home directory but more logicaly in my projects folder. Another aim is to help separate Bash configuration and System installation, because linking files isn't in the install script anymore
+# This shell script creates symbolic links to Bash configuration files, so the "Git/Dotfiles" git repo isn't at the root of my Home directory but more logicaly in my projects folder. Another aim is to help separate Bash configuration and System installation, because linking files isn't in the install script anymore
 
 
-echo "Deploying dotfiles..."
+echo "Deploying Dotfiles..."
 
 # Dotfiles deployment
 mkdir ~/.bash
-ln -sfv "~/.dotfiles/conf/bash_profile" "~/.bash_profile"
-ln -sfv "~/.dotfiles/conf/aliases" "~/.bash/aliases"
-ln -sfv "~/.dotfiles/conf/exports" "~/.bash/exports"
-ln -sfv "~/.dotfiles/conf/extras" "~/.bash/extras"
-ln -sfv "~/.dotfiles/conf/functions" "~/.bash/functions"
-ln -sfv "~/.dotfiles/conf/inputrc" "~/.bash/inputrc"
-ln -sfv "~/.dotfiles/conf/path" "~/.bash/path"
-ln -sfv "~/.dotfiles/conf/prompt" "~/.bash/prompt"
+ln -s ~/Digital\ Creation/Dotfiles/conf/bash_profile ~/.bash_profile
+ln -s ~/Digital\ Creation/Dotfiles/conf/aliases ~/.bash/aliases
+ln -s ~/Digital\ Creation/Dotfiles/conf/exports ~/.bash/exports
+ln -s ~/Digital\ Creation/Dotfiles/conf/extras ~/.bash/extras
+ln -s ~/Digital\ Creation/Dotfiles/conf/functions ~/.bash/functions
+ln -s ~/Digital\ Creation/Dotfiles/conf/inputrc ~/.bash/inputrc
+ln -s ~/Digital\ Creation/Dotfiles/conf/path ~/.bash/path
+ln -s ~/Digital\ Creation/Dotfiles/conf/prompt ~/.bash/prompt
 
-ln -sfv "~/.dotfiles/gitignore" "~/.gitignore"
+ln -s ~/Git/Dotfiles/conf/gitignore ~/Gitignore
 
-ln -sfv "~/.dotfiles/conf/mackup.cfg" "~/.mackup.cfg"
+ln -s ~/Git/Dotfiles/conf/mackup.cfg ~/.mackup.cfg
 
 # Done
 echo "Done. That was fast!"
